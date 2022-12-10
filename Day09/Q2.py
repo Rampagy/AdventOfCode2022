e4bc3b1b968d8dac6054c55b150be9596764ba6b
Rampagy <alexjsimon32@gmail.com>
2022-12-09 19:43:07 -0600

    Day 9

--- Day09/Q2.py
+++ Day09/Q2.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     all_tail_locations = [knots[-1]]
 
-    with open('test1.txt', 'r') as f:
+    with open('input.txt', 'r') as f:
         for i, line in enumerate(f):
             [direction, distance] = helpers.ParseLine('{} {:d}\n', line)
 
@@ -42,44 +42,60 @@ if __name__ == '__main__':
 
                 for j in range(1, NUMBER_KNOTS):
                     #separation = helpers.heuristic(knots[j], knots[j-1], allow_diagonals=True)
-                    deltax = abs(knots[j].x - knots[j-1].x)
-                    deltay =  abs(knots[j].y - knots[j-1].y)
+                    deltax = abs(knots[j].x - knots[j-1].x) # 1
+                    deltay =  abs(knots[j].y - knots[j-1].y) # 2
                     if (deltax == 2 and deltay == 0) or (deltay == 2 and deltax == 0):
-                        knots[j] = helpers.Position(knots[j].x + delta.x, knots[j].y + delta.y)
+
+                        tempdeltax = knots[j-1].x - knots[j].x
+                        if tempdeltax > 1:
+                            tempdeltax = 1
+                        elif tempdeltax < -1:
+                            tempdeltax = -1
+
+                        tempdeltay = knots[j-1].y - knots[j].y
+                        if tempdeltay > 1:
+                            tempdeltay = 1
+                        elif tempdeltay < -1:
+                            tempdeltay = -1
+
+                        #knots[j] = helpers.Position(knots[j].x + delta.x, knots[j].y + delta.y)
+                        knots[j] = helpers.Position(knots[j].x + tempdeltax, knots[j].y + tempdeltay)
+
                     elif (deltax == 2 and deltay == 1) or (deltax == 1 and deltay == 2): # and (knots[j].x != knots[j-1].x or knots[j].y != knots[j-1].y):
                         # if the head and tail are greater than 1.5 away from each other
                         # update the tails position to the previous head position
                         # diagonals will be 1.414 apart
                         tempdeltax = knots[j-1].x - knots[j].x
-                        if tempdeltax >= 2:
+                        if tempdeltax > 1:
                             tempdeltax = 1
-                        elif tempdeltax <= -2:
+                        elif tempdeltax < -1:
                             tempdeltax = -1
 
                         tempdeltay = knots[j-1].y - knots[j].y
-                        if tempdeltay >= 2:
+                        if tempdeltay > 1:
                             tempdeltay = 1
-                        elif tempdeltay <= -2:
+                        elif tempdeltay < -1:
                             tempdeltay = -1
 
                         knots[j] = helpers.Position(knots[j].x + tempdeltax, knots[j].y + tempdeltay)
 
-                    elif deltax == 2 and deltay == 2:
+                    elif deltax > 1 and deltay > 1:
                         tempdeltax = knots[j-1].x - knots[j].x
-                        if tempdeltax >= 2:
+                        if tempdeltax > 1:
                             tempdeltax = 1
-                        elif tempdeltax <= -2:
+                        elif tempdeltax < -1:
                             tempdeltax = -1
 
                         tempdeltay = knots[j-1].y - knots[j].y
-                        if tempdeltay >= 2:
+                        if tempdeltay > 1:
                             tempdeltay = 1
-                        elif tempdeltay <= -2:
+                        elif tempdeltay < -1:
                             tempdeltay = -1
 
                         knots[j] = helpers.Position(knots[j].x + tempdeltax, knots[j].y + tempdeltay)
+
 
                 all_tail_locations += [copy.deepcopy(knots[-1])]
-            print(knots)
+            #print(direction, distance, knots)
 
     print(len(set(all_tail_locations)))
